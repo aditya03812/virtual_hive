7e17c20845f312eacb86a1e7299845cdd19cebc9
Aditya Kumar <107626853+aditya03812@users.noreply.github.com>
2025-05-14 20:35:54 +0530

    Update index.ts

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,19 +2,25 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-const cached = (global as any).mongoose || { conn: null, promise: null };
+// Ensure the global object has a 'mongoose' property
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'virtual',
+    dbName: 'evently',
     bufferCommands: false,
-  })
+  });
 
   cached.conn = await cached.promise;
 
   return cached.conn;
-}
+};
+

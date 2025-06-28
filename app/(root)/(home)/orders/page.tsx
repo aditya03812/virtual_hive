e6f421844893214d11510381af711c1e36a49716
aditya03812 <107626853+aditya03812@users.noreply.github.com>
2025-06-29 01:46:11 +0530

    part 1 completed

--- app/(root)/(home)/orders/page.tsx
+++ app/(root)/(home)/orders/page.tsx
@@ -22,7 +22,7 @@ const Orders = async (props: SearchParamProps) => {
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <h3 className="wrapper h3-bold text-center sm:text-left">Orders</h3>
+        <h3 className="wrapper h3-bold text-white text-center sm:text-left">Orders</h3>
       </section>
 
       <section className="wrapper mt-8">

1661e8200ce0a1e8532cf79c2cd7cce69da746a8
aditya03812 <107626853+aditya03812@users.noreply.github.com>
2025-06-28 21:28:18 +0530

    cleark fixing

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,30 +1,14 @@
-import { WebhookEvent } from '@clerk/nextjs/server';
-import { headers } from 'next/headers';
-import { Webhook } from 'svix';
-import { NextRequest, NextResponse } from 'next/server';
-
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.action';
 import { clerkClient } from '@clerk/clerk-sdk-node';
 
-const webhookSecret = process.env.CLERK_WEBHOOK_SECRET || '';
-
-async function validateRequest(request: Request) {
-  const payload = await request.text();
-  const headerPayload = await headers();
-
-  const svixHeaders = {
-    'svix-id': headerPayload.get('svix-id')!,
-    'svix-timestamp': headerPayload.get('svix-timestamp')!,
-    'svix-signature': headerPayload.get('svix-signature')!,
-  };
-
-  const wh = new Webhook(webhookSecret);
-  return wh.verify(payload, svixHeaders) as WebhookEvent;
-}
+import { verifyWebhook } from '@clerk/nextjs/webhooks';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
   try {
-    const evt = await validateRequest(req);
+    const evt = await verifyWebhook(req);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id } = evt.data;
     const eventType = evt.type;
 
     if (eventType === 'user.created') {
@@ -50,41 +34,41 @@ export async function POST(req: NextRequest) {
 
       if (newUser) {
         await clerkClient.users.updateUser(id, {
-          publicMetadata: {
-            userId: newUser._id,
-          },
-        });
+            publicMetadata: {
+              userId: newUser._id,
+            },
+          });
       }
 
-      return NextResponse.json({ message: 'User created', user: newUser });
+      return NextResponse.json({ message: 'OK', user: newUser });
     }
 
     if (eventType === 'user.updated') {
-      const { id, image_url, first_name, last_name, username } = evt.data;
-
-      const user = {
-        firstName: first_name,
-        lastName: last_name,
-        username: username!,
-        photo: image_url,
-      };
-
-      const updatedUser = await updateUser(id, user);
-
-      return NextResponse.json({ message: 'User updated', user: updatedUser });
-    }
-
-    if (eventType === 'user.deleted') {
-      const { id } = evt.data;
-
-      const deletedUser = await deleteUser(id!);
-
-      return NextResponse.json({ message: 'User deleted', user: deletedUser });
-    }
+        const {id, image_url, first_name, last_name, username } = evt.data
+    
+        const user = {
+          firstName: first_name,
+          lastName: last_name,
+          username: username!,
+          photo: image_url,
+        }
+    
+        const updatedUser = await updateUser(id, user)
+    
+        return NextResponse.json({ message: 'OK', user: updatedUser })
+      }
+    
+      if (eventType === 'user.deleted') {
+        const { id } = evt.data
+    
+        const deletedUser = await deleteUser(id!)
+    
+        return NextResponse.json({ message: 'OK', user: deletedUser })
+      }
 
     return NextResponse.json({ message: 'Unhandled event type' });
   } catch (err) {
-    console.error('❌ Webhook validation failed:', err);
-    return new Response('Webhook validation failed', { status: 400 });
+    console.error('Error verifying webhook:', err);
+    return new Response('Error verifying webhook', { status: 400 });
   }
 }
